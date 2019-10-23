f819b78fbddf4271b504ecba4514d004a929f318
Kevin Kim <kevin.kim1030@gmail.com>
2019-10-23 22:10:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,13 @@
 def nyc_pigeon_organizer(data)
   final_hash = {}
   data.each do |attribute_name, attributes|
-    pigeon_names.each do |name|
-      pigeon_list[name] || = {}
-      pigeon_list[name][attribute_name] || = []
+    attributes.each do |attribute_value, pigeon_names|
+      pigeon_names.each do |name|
+        pigeon_list[name] || = {}
+        pigeon_list[name][attribute_name] || = []
+        pigeon_list[name][attribute_name].push(attribute_value)
+      end
     end
   end
+  final_hash
 end
